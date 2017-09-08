88f2d0126df5db141e6bfaa8a66130da309d3cc7
Ishaan 2 <ishjain321@gmail.com>
2017-09-08 14:06:46 -0400

    Initial commit

---
+++ ngram.py
@@ -0,0 +1,54 @@
+import os
+import operator
+
+#open file
+filepath = os.path.abspath(".")
+fp = open(filepath + "\SentimentDataset\Dev\pos.txt", 'r')
+fn = open(filepath + "\SentimentDataset\Dev\\"+"neg.txt", 'r')
+
+#read corpora
+pos = fp.read()
+pos = pos.split()
+neg = fn.read()
+neg = neg.split()
+
+#make uni and bigram
+def ngram(s):
+    unigram = {}
+    bigram = {}
+
+    #calculate counts
+    for i in s:
+        if unigram.get(i, "") == "":
+            unigram[i] = 1.0
+        else:
+            unigram[i] += 1.0
+    for idx,_ in enumerate(s[1:]):
+        big = s[idx]+"|"+s[idx-1]
+        if bigram.get(big, "") == "":
+            bigram[big] = 1.0
+        else:
+            bigram[big] += 1.0
+
+    #calculate probabilities
+    for i in bigram:
+        bigram[i] /= unigram[i.split("|")[1]]
+    for i in unigram:
+        unigram[i] /= len(unigram)
+
+    return unigram, bigram
+
+#get the ngrams
+unipos, bipos = ngram(pos)
+unineg, bineg = ngram(neg)
+
+#Sort the ngrams
+unipos = sorted(unipos.items(), key=operator.itemgetter(1), reverse = True)
+bipos = sorted(bipos.items(), key=operator.itemgetter(1), reverse = True)
+unineg = sorted(unineg.items(), key=operator.itemgetter(1), reverse = True)
+bineg = sorted(bineg.items(), key=operator.itemgetter(1), reverse = True)
+
+print(unipos)
+print(bipos)
+print(unineg)
+print(bineg)
