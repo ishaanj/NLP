af1fb2be18652fb6e3f5475f28dde1001781126e
Nithish R <rnithish@outlook.com>
2017-10-09 14:31:52 -0400

    adding HMM

--- Project_2/2707_Team/NER.py
+++ Project_2/2707_Team/NER.py
@@ -1,3 +1,12 @@
+#Need count(wordType,NER)
+#Need count(NER)
+#Need count(NER,NER)
+
+#will keep a dummy start NER and there will be a count(start NER, NER) and a count(start NER) = #of lines
+
+
+import nltk
+import  numpy as np
 def tokenize(filename="train.txt"):
     """
     Tokenizes the train file and returns
@@ -9,6 +18,11 @@ def tokenize(filename="train.txt"):
     all_tokens = {}
     i_tokens = {}
     o_tokens = {}
+    count_wordType_NER = {}
+    count_NER_NER = {}
+    count_NER = {}
+    count_NER["START"] = 0 #useful later - refer comments
+
 
     all_lines = f.readlines()
     for i in range(int(len(all_lines)/3)):
@@ -20,8 +34,98 @@ def tokenize(filename="train.txt"):
             if "O" != line_ner[j]:
                 if line_tokens[j] not in all_tokens:
                     all_tokens[line_tokens[j]] = line_ner[j]
+            #leaving above unchanged and adding new code here
+            wordType = line_tokens[j]
+            NER = line_ner[j]
+
+            if((wordType,NER) not in count_wordType_NER):
+                count_wordType_NER[(wordType,NER)] = 1
+            else:
+                count_wordType_NER[(wordType, NER)] += 1
+            if (j == 0):
+                prevNER = "START"
+                count_NER[prevNER] += 1 #already initialized to zero above
+            else:
+                prevNER = line_ner[j - 1]
+            if ((prevNER, NER) not in count_NER_NER):
+                count_NER_NER[(prevNER, NER)] = 1
+            else:
+                count_NER_NER[(prevNER, NER)] += 1
+            if(NER not in count_NER):
+                count_NER[NER] = 1
+            else:
+                count_NER += 1
+
     f.close()
-    return all_tokens
+    return all_tokens, count_NER, count_NER_NER, count_wordType_NER
+
+
+def HMM(line_tokens):
+    """
+        Predicts NER for the test data sentence(line_tokens)
+        Each row will have info about the i'th word in the sentence
+        Each row will only have 5 cols. 0-PER 1-LOC 2-ORG 3-MISC 4-O
+        Using +1 smoothing, so k = 1
+        :param line_tokens: one line from test.txt
+        :return: None
+    """
+    hmatrix = []
+    ptrmatrix = []
+    NER_Types = ["PER","LOC","ORG","MISC", "O"]
+    word_types = len(ALL_TOKENS)
+    ner = 5
+    k = 1
+
+    start_word = line_tokens[0]
+    cur_row = []
+    for NER_Type in NER_Types:
+        num = count_NER_NER[("START",NER_Type)] if ("START",NER_Type) in count_NER_NER else 0
+        den = count_NER["START"]
+        trans_prob = float((num + k)/(den+ner*k))
+        num = count_wordType_NER[(start_word,NER_Type)] if (start_word,NER_Type) in count_wordType_NER else 0
+        den = count_NER[NER_Type]
+        gen_prob = float((num + k)/(den+ner*k))
+        cur_row.append(trans_prob*gen_prob)
+    hmatrix.append(cur_row)
+
+    for j in range(1,len(line_tokens)):
+        prev_row = hmatrix[j-1]
+        cur_word = line_tokens[j]
+        cur_row = []
+        ptr_row = []
+        for NER_Type in NER_Types:
+            tempspace = []
+            for p in range(len(prev_row)):
+                prev_NER_Type = NER_Types[p]
+                prev_score = prev_row[p]
+
+                num = count_NER_NER[(prev_NER_Type, NER_Type)] if (prev_NER_Type, NER_Type) in count_NER_NER else 0
+                den = count_NER[prev_NER_Type]
+                trans_prob = float((num + k) / (den + ner * k))
+                num = count_wordType_NER[(cur_word, NER_Type)] if (cur_word, NER_Type) in count_wordType_NER else 0
+                den = count_NER[NER_Type]
+                gen_prob = float((num + k) / (den + ner * k))
+                tempspace.append(prev_score*trans_prob*gen_prob)
+
+            cur_row.append(max(tempspace))
+            ptr_row.append(np.argmax(tempspace))
+        hmatrix.append(cur_row)
+        ptrmatrix.append(ptr_row)
+
+    result = []
+    idx = np.argmax(hmatrix[len(line_tokens)-1])
+    bptr = ptrmatrix[len(line_tokens)-1][idx]
+    result.append(NER_Types[idx])
+    for p in range(len(line_tokens)-2,0,-1):
+        result.append(NER_Types[bptr])
+        bptr = ptrmatrix[p][bptr]
+
+    result = result[::-1]
+    return result
+
+
+
+
 
 
 def predict_NER(filename="test.txt", output_csv="output.csv"):
@@ -42,6 +146,7 @@ def predict_NER(filename="test.txt", output_csv="output.csv"):
         line_tokens = all_lines[3*i].split()
         line_pos = all_lines[3*i + 1].split()
         token_number = all_lines[3*i + 2].split()
+        tagged_tokens = HMM(line_tokens)
         j = 0
         while(j<len(line_tokens)):
             if line_tokens[j] in ALL_TOKENS:
@@ -92,5 +197,21 @@ def predict_NER(filename="test.txt", output_csv="output.csv"):
 
 
 # Main calls
-ALL_TOKENS = tokenize("train.txt")
+ALL_TOKENS, count_NER, count_NER_NER, count_wordType_NER = tokenize("train.txt")
 predict_NER('test.txt', "output.csv")
+
+f = open('train.txt')
+raw = f.read()
+
+tokens = nltk.word_tokenize(raw)
+
+#Create your bigrams
+#bgs = nltk.bigrams(tokens)
+ugs = nltk.ngrams(tokens,1)
+#tgs = nltk.trigrams(tokens)
+#fgs = nltk.ngrams(tokens,4)
+#compute frequency distribution for all the bigrams in the text
+fdist = nltk.FreqDist(ugs)
+#tdist = nltk.FreqDist(fgs)
+for k,v in fdist.items():
+    print(k,v)
