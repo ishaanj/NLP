108933ceae7a42a432f3fad164861a12e23f8d4b
Akash Peri <akash.peri@gmail.com>
2017-10-26 22:59:58 -0400

    get first answer through, though it's all considered as a single sentence...

--- Project_3/2608/QA.py
+++ Project_3/2608/QA.py
@@ -1,6 +1,7 @@
 import json
 import re
 import string
+from scipy import spatial
 
 def read_from_file(filename):
     with open(filename) as raw_data:
@@ -14,7 +15,9 @@ def answer_questions(question_data):
             context = paragraph['context']
             normalized_context = normalize_context(context)
             for qa in paragraph['qas']:
+                # bag_of_word_vectors = get_bag_of_word_vectors(normalized_context)
                 answers = solve_question(title, context, normalized_context, qa)
+                print answers
 
 def normalize_context(s):
     """Lower text and remove punctuation, articles and extra whitespace."""
@@ -33,20 +36,57 @@ def normalize_context(s):
 
     return white_space_fix(remove_articles(remove_punc(lower(s))))
 
+def get_similarity(sentence_1, sentence_2):
+    bag_of_words = set([])
+    shared_words = 0
+
+    for ele in sentence_1.split():
+        # print "s1 " + ele
+        bag_of_words.add(ele)
+
+    for ele in sentence_2.split():
+        # print "s2 " + ele
+        if ele in bag_of_words:
+            shared_words+=1
+
+    return shared_words
+
 def solve_question(title, context, normalized_context, qa):
-    context_arr = normalized_context.split()
-    window_size = 10
-    word_dict
 
-    if len(context_arr) > window_size:
+    question = normalize_context(qa['question'])
+    highest_similarity_score = None
+    highest_similarity_sentence = None
 
-        for i in range(0, len(context_arr)-window_size):
-            #now, use sliding window approach
-            #window goes from i to i+10, just count up all the words in the window
-            window = context_arr[i:i+window_size]
+    context_arr = normalized_context.split('.')
+    for ele in context_arr:
+        print "potential elements: "
+        print ele
+        #this is now every sentence
+        similarity = get_similarity(question, ele)
+        print similarity
+        if highest_similarity_score is None or highest_similarity_score < similarity:
+            highest_similarity_score = similarity
+            highest_similarity_sentence = ele
 
+    print highest_similarity_score
+    print highest_similarity_sentence
 
+    raise Exception()
 
+    # best_word_vector_score = None
+    # best_word_vector = None
+    #
+    # for word_vector in bag_of_word_vectors:
+    #     print word_vector
+    #     print qa
+    #     diff_in_length = len(word_vector.values()) - len(qa.values())
+    #     result = spatial.distance.cosine(qa.values().extend([0]*diff_in_length), word_vector.values())
+    #     if(best_word_vector_score is None or best_word_vector_score > result):
+    #         best_word_vector_score = result
+    #         best_word_vector = word_vector
+    #
+    # return best_word_vector
 
 question_data = read_from_file("development.json")
+answer_questions(question_data)
 
